d5c6fed578591f8052b863b57d3ce51a5a9eb265
Amr Magdy <amagdy195@yahoo.com>
2022-03-26 16:30:25 +0200

    Initial Commit

---
+++ BarakaStockMarket/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  BarakaStockMarket
+//
+//  Created by Amr Magdy on 26/03/2022.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
